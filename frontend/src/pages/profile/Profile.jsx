6b44075a406ef10887621976e400efab1868ccf4
oks1 <frezia2010@yahoo.com>
2022-12-05 09:26:59 -0500

    create post popup window + emoji (need to run npm i)

--- frontend/src/pages/profile/Profile.jsx
+++ frontend/src/pages/profile/Profile.jsx
@@ -9,66 +9,68 @@ import { useParams } from "react-router";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../utils/AuthContext";
 
-export default function Profile() {
+export default function Profile({ setVisible }) {
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
   const [user, setUser] = useState({});
   const { username } = useParams();
   const { authState } = useContext(AuthContext);
-  
+
   // console.log("user name on profile", username)
   // console.log("AuthState on profile:", authState)
- 
+
   useEffect(() => {
-  const fetchUser = async () => {
-    const res = await axios.get(`users?username=${username}`);
-    //console.log(res) 
-    setUser(res.data);
-  };
-  //console.log("User in useEffect :", user);
-  //window.location.reload();
-fetchUser();
+    const fetchUser = async () => {
+      const res = await axios.get(`users?username=${username}`);
+      //console.log(res)
+      setUser(res.data);
+    };
+    //console.log("User in useEffect :", user);
+    //window.location.reload();
+    fetchUser();
+  }, [username]);
 
-}, [username]); 
+  //TO CHECK IF ITS MY OWN PROFILE
+  let itsMyProfile = false;
+  if (authState.username === username) {
+    itsMyProfile = true;
+  }
 
-//TO CHECK IF ITS MY OWN PROFILE
-let itsMyProfile=false;
-if(authState.username === username){
-  itsMyProfile = true;
-}
+  let areFriends = false;
+  // console.log("AuthState before FOR:", authState);
+  //console.log("User before FOR:", user);
+  // console.log(Object.keys(user).length);
 
-
-let areFriends = false;
-// console.log("AuthState before FOR:", authState);
-//console.log("User before FOR:", user);
-// console.log(Object.keys(user).length);
-
-if(Object.keys(user).length !== 0){
+  if (Object.keys(user).length !== 0) {
     for (let i = 0; i < authState.friendships.length; i++) {
-    for (let j = 0; j < user.friendships.length; j++) {
-     // console.log("current user", authState.friendships);
-     // console.log("profile user", user.friendships);
-      if (authState.friendships[i] === user.friendships[j]) {
-        areFriends = true;
-        break;
-       // console.log("hello");
+      for (let j = 0; j < user.friendships.length; j++) {
+        // console.log("current user", authState.friendships);
+        // console.log("profile user", user.friendships);
+        if (authState.friendships[i] === user.friendships[j]) {
+          areFriends = true;
+          break;
+          // console.log("hello");
+        }
       }
     }
   }
-} 
 
-//button to add a friend
-const addFriend=(id)=>{
-  axios.post("api/friends/user/addFriend",
-  {
-    friend: id,
-  },
-  {
-    headers: { accessToken: localStorage.getItem("accessToken") },
-  }).then(() => {
-    window.location.reload();
-    // navigate("/");
-    }
-  )}
+  //button to add a friend
+  const addFriend = (id) => {
+    axios
+      .post(
+        "api/friends/user/addFriend",
+        {
+          friend: id,
+        },
+        {
+          headers: { accessToken: localStorage.getItem("accessToken") },
+        }
+      )
+      .then(() => {
+        window.location.reload();
+        // navigate("/");
+      });
+  };
 
   return (
     <>
@@ -92,26 +94,29 @@ const addFriend=(id)=>{
             <div className="profileInfo">
               <h4 className="profileInfoName">{user.username}</h4>
 
-              {itsMyProfile ?(
-               <Link to={`/aboutMe/${user._id}`}>About me</Link>
-              ):(
+              {itsMyProfile ? (
+                <Link to={`/aboutMe/${user._id}`}>About me</Link>
+              ) : (
                 <span></span>
               )}
-           
-              {!areFriends ?(
-              <button onClick={() => {
-                      addFriend(user._id);
-                    }}>Add friend</button>                    
-                   
-                   ):(
-                      <span></span>
-                      //<Link to={`/aboutMe/${user._id}`}>About me</Link>
-                    )}
+
+              {!areFriends ? (
+                <button
+                  onClick={() => {
+                    addFriend(user._id);
+                  }}
+                >
+                  Add friend
+                </button>
+              ) : (
+                <span></span>
+                //<Link to={`/aboutMe/${user._id}`}>About me</Link>
+              )}
               <span className="profileInfoDesc">{user.desc}</span>
             </div>
           </div>
           <div className="profileRightBottom">
-            <Feed username={username} />
+            <Feed username={username} setVisible={setVisible} />
             <Rightbar user={user} />
           </div>
         </div>
